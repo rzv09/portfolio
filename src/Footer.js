6c9f19aa89ef8c21c4fce188d9af862361f8a23b
Raman Z <rzv090701@gmail.com>
2023-03-07 22:14:56 -0500

    favicon

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
-        <div class="contact-info" id="footer">
-        <a class="footer-link" href="https://www.linkedin.com/in/raman-zatsarenko/">LinkedIn</a>
-        <a class="footer-link" href="https://github.com/rzv09">Github</a>
-        <a class="footer-link" href="https://t.me/rzv0_0">Telegram</a>
-        <p class="copyright">© 2023 Raman Zatsarenko.</p>
+        <div className="contact-info" id="footer">
+        <a className="footer-link" href="https://www.linkedin.com/in/raman-zatsarenko/">LinkedIn</a>
+        <a className="footer-link" href="https://github.com/rzv09">Github</a>
+        <a className="footer-link" href="https://t.me/rzv0_0">Telegram</a>
+        <p className="copyright">© 2023 Raman Zatsarenko.</p>
         </div>
     );
 }
